4a61794169ed9499cddef91cc9535dc3f7eef692
Sergio Borne <sergio.borne@fleetmatics.com>
2018-04-09 14:49:17 +0100

    Adding leak canary to find memory leaks

--- app/src/main/java/com/sergioborne/themoviedb/TMDBApplication.kt
+++ app/src/main/java/com/sergioborne/themoviedb/TMDBApplication.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.app.Application
 import com.facebook.stetho.Stetho
 import com.sergioborne.themoviedb.di.DaggerAppComponent
+import com.squareup.leakcanary.LeakCanary
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
@@ -24,6 +25,16 @@ class TMDBApplication : Application(), HasActivityInjector {
         .inject(this)
 
     initStethoIfDebug()
+    initLeakCanary()
+  }
+
+  private fun initLeakCanary() {
+    if (LeakCanary.isInAnalyzerProcess(this)) {
+      // This process is dedicated to LeakCanary for heap analysis.
+      // You should not init your app in this process.
+      return
+    }
+    LeakCanary.install(this);
   }
 
   private fun initStethoIfDebug() {
